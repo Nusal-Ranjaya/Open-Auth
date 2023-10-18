ae256f2235ffae93b8f4f339e3b2c35db172e4e5
Nusal-Ranjaya <muthuthanthrige@gmail.com>
2023-10-19 00:11:46 +0530

    Initial

---
+++ dashboard.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Dashboard</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <h1>You have been Authenticatd</h1>
+    </body>
+</html>
